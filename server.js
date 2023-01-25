1f4378aeeff7b063b02ce3de9e3a960005cc1739
rastack <rastack@email.unc.edu>
2023-01-25 15:40:48 -0500

    Syntax changes

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const port = args.port || 3000;
 // Do not be nice about exiting.
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
 	if (err) {
-		console.error(err);
-		return;
+	  console.error(err);
+	  return;
 	}
 	console.log(data);
 // Define a const `server` as an arrow function using http.createServer. 
@@ -29,14 +29,14 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 	const server = http.createServer((req, res) => {
-		res.writeHead(200)
-		res.setHeader('Content-Type', 'text/html')
-		res.end(data)
+	    res.statusCode(200)
+	    res.setHeader('Content-Type', 'text/html')
+	    res.end(data)
 	})
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 	server.listen(port, () => {
-		console.log('Server listening on port ${port}')
+	    console.log('Server listening on port' ${port})
 	})
 });
 // That's it! You're all done!
